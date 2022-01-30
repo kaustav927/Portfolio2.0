d83df65ce4e97b606f241903ade5ca5d3451d4cc
kaustav927 <kaustav927@gmail.com>
2022-01-29 23:03:27 -0500

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import InputField from '../components/inputFeild'
 import styles from '../styles/Home.module.css'
-
+import Nav from '../components/Nav'
 const Home: NextPage = () => {
 
 
     return (
+        
+
         <div>
-            <InputField/> 
-            <h1>Hello </h1>
+            <div>
+                <Nav></Nav>
+            </div>
         </div>
         
       
