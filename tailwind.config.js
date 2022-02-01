27b422fbd6884863f6c680fe814613903c3344fc
kaustav927 <kaustav927@gmail.com>
2022-02-01 14:21:14 -0500

    tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body':'#17171F',
+        'selected-text':'#A3A3FF',
+        'theme':'#3F3FFF',
+        'nav':'#404053',
+        'secondary':'#9191A4',
+        'badge':'#3F3F51',
+        'input-border':'#565666',
+        'input':'#2A2A35',
+
+      }
+    },
   },
   plugins: [],
 }
