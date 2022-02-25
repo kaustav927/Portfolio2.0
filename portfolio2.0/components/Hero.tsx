6244b2e6a4f4b0b82c866a5e79c2dba73c9546c2
kaustav927 <kaustav927@gmail.com>
2022-02-25 18:32:15 -0500

    style

--- portfolio2.0/components/Hero.tsx
+++ portfolio2.0/components/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
             <div className="flex flex-wrap ml-5 lg:ml-20 justify-center md:justify-start max-w-xl mt-0 md:my-10">
             <div className="relative w-full max-w-lg">
             <h1 className="font-cd-regular mr-7 text-2xl my-10 md:text-6xl lg:text-7xl text-center md:text-left">Building beautiful web experiences.</h1>
-                <div className="absolute top-0 -left-4 w-72 h-72 bg-slate-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
+                <div className="absolute top-0 -left-4 w-72 h-72 bg-copper-rust rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
                 <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-2000"></div>
                 <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-4000"></div>
             </div>
--- portfolio2.0/pages/index.tsx
+++ portfolio2.0/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       <div className="text-black">
       <Head>
         <title>Kaustav Sharma</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"  />
       </Head>
 
       <Navbar/>
