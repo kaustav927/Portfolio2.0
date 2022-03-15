61c86b76e767dc268f50b1958638010a00dd7744
kaustav927 <kaustav927@gmail.com>
2022-03-14 23:55:35 -0400

    old components

---
+++ portfolio2.0/components/Hero.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="container mt-16 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full ">
+      <div className="flex flex-wrap md:flex-nowrap">
+            <div className="flex flex-wrap ml-5 lg:ml-20 justify-center md:justify-start max-w-xl mt-0 md:my-10">
+            <div className="relative w-full max-w-lg">
+            <h1 className="font-cd-regular mr-7 text-2xl my-10 md:text-6xl lg:text-7xl text-center md:text-left">Building beautiful web experiences.</h1>
+                <div className="absolute top-0 -left-4 w-72 h-72 bg-slate-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
+                <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-2000"></div>
+                <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-4000"></div>
+            </div>
+               
+                <div className="w-full flex justify-center md:justify-start">
+                    <button className="mr-6 py-4 bg-theme text-bla font-bold mt-12 flex items-center space-x-3">
+                        <span className="font-cd-regular">View my work.</span>
+                    </button>
+                </div>
+            </div>
+      {/* image */}
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ portfolio2.0/components/Navbar.tsx
@@ -0,0 +1,76 @@
+import React, {useState} from 'react'
+
+
+
+function NavLink({to, children}) {
+    return <a href={to} className={`mx-4`}>
+    {children}
+    </a> 
+}
+
+//https://dev.to/dmuraco3/responsive-global-navbar-in-nextjs-with-tailwindcss-45p4
+function MobileNav({open, setOpen}) {
+    return (
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-cool-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter  `}>
+            <div className="flex items-center justify-center filter bg-cool-white h-20"> {/*logo container*/}
+                <a className="text-xl font-cd-regular " href="/">Kaustav Sharma</a>
+            </div>
+            <div className="flex flex-col ml-4">
+                <a className="text-sm font-medium my-4" href="#about" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    About
+                </a>
+                <a className="text-sm font-normal my-4" href="#projects" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Projects
+                </a>
+                <a className="text-sm font-normal my-4" href="#contact" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Contact
+                </a>
+                <a className="text-sm font-normal my-4 bg-slate-800 py-2 px-4 rounded text-white mx-auto" href="https://drive.google.com/file/d/18XyT6wPX2IWdWv0VCqOtwW7ZXYSuAm_g/view?usp=sharing" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Resume
+                </a>
+            </div>  
+        </div>
+    )
+}
+
+export default function Navbar() {
+    const [open, setOpen] = useState(false)
+    return (
+        <nav className="font-cd-regular flex container mx-auto justify-between py-4 my-8 h-20 items-center px-8 md:px-14 lg-px24 w-full">
+            <MobileNav open={open} setOpen={setOpen}/>
+
+            <div className="w-4/12 flex items-center">
+                <a className="text-2xl font-cd-regular py-2 " href="/">Kaustav Sharma</a>
+            </div>
+            <div className="w-9/12 flex justify-end items-center">
+
+                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
+                    setOpen(!open)
+                }}>
+                    {/* hamburger button */}
+                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                </div>
+
+                <div className="hidden md:flex">
+                    <NavLink to="#about">
+                       <h1 className="text-sm  py-2 px-4">About</h1>
+                    </NavLink>
+                    <NavLink to="#projects">
+                        <h1 className="text-sm  py-2 px-4">Projects</h1>
+                    </NavLink>
+                    <NavLink to="#contact">
+                        <h1 className="text-sm  py-2 px-4">Contact</h1>
+                    </NavLink>
+                    <NavLink to="https://drive.google.com/file/d/18XyT6wPX2IWdWv0VCqOtwW7ZXYSuAm_g/view?usp=sharing">
+                        <h1 className="text-lg bg-slate-800 py-2 px-4 rounded text-white hover:bg-slate-800/3">Resume</h1>
+                    </NavLink>
+                </div>
+
+
+            </div>
+        </nav>
+    )
+
+}
