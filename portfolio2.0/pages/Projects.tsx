6498b71ae8adadd3245682ec477055e42c8612ab
kaustav927 <kaustav927@gmail.com>
2022-04-09 23:20:26 -0400

    been a min

--- portfolio2.0/pages/Projects.tsx
+++ portfolio2.0/pages/Projects.tsx
@@ -6,10 +6,10 @@ function Projects() {
     <div>
       <div className="container mt-64 flex justify-between items-center mx-auto p-8 md:px-14 lg:px-24 w-full">
 			<section className="w-full">
-				<h2 id="work" className="secondary-title">My work</h2>
-				<p className="section-paragraph">I’ve had the pleasure of working with multiple Fortune 500 companies, designing and implementing both frontend and backend.</p>
+				<h2 id="work" className="text-2xl font-cd-bold">My work</h2>
+				<p className="font-cd-regular">I’ve had the pleasure of working with multiple Fortune 500 companies, designing and implementing both frontend and backend.</p>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+				<div className="flex flex-col items-center gap-6 mt-6">
                     <div className ="w-full bg-nav h-36 lg:h-72 object-cover">image</div>
                     <div className ="w-full bg-nav h-36 lg:h-72 object-cover">image</div>
                     <div className ="w-full bg-nav h-36 lg:h-72 object-cover">image</div>
