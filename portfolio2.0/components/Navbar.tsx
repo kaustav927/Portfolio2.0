8c79523e65acaafe26063958ec8e8c869467f920
kaustav927 <kaustav927@gmail.com>
2022-04-26 02:17:00 -0400

    dark mode enhancements

--- portfolio2.0/components/Navbar.tsx
+++ portfolio2.0/components/Navbar.tsx
@@ -12,27 +12,30 @@ function NavLink({to, children}) {
     </a>
 }
 
-function MobileNav({open, setOpen}) {
+function MobileNav({open, setOpen, theme,setTheme}) {
     return (
-        <div className={`absolute top-0 left-0 h-screen w-screen bg-cool-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter  `}>
-            <div className="flex items-center justify-center filter  bg-cool-white h-20"> {/*logo container*/}
-                <a className="text-xl font-cd-semibold" href="/">LOGO</a>
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-cool-white/70 dark:bg-slate-800/70 bg-blur-md transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter  `}>
+            <div className="flex items-center justify-center filter  bg-cool-white dark:bg-slate-800 h-20"> {/*logo container*/}
+                <a className="text-xl font-cd-semibold dark:text-white" href="/">@kaust_av</a>
             </div>
             <div className="flex flex-col ml-4">
+                    <h1 className="text-2xl mt-1.5 mx-auto">
+                        {theme==='light' ? <BsFillMoonFill className="text-slate-800" onClick={()=>setTheme(theme === 'light'? 'dark':'light')}/> : <BsFillSunFill className="text-cool-white" onClick={()=>setTheme(theme === 'light'? 'dark':'light')}/>}
+                    </h1>
                     <NavLink to="#about">
-                        <h1 className="text-md font-cd-semibold dark:text-white" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>About</h1>
+                        <h1 className="text-md font-cd-semibold dark:text-white py-4" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>About</h1>
                     </NavLink>
                     <NavLink to="#projects">
-                        <h1 className="text-sm font-cd-semibold dark:text-white" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>Projects</h1>
+                        <h1 className="text-sm font-cd-semibold dark:text-white py-4" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>Projects</h1>
                     </NavLink>
                     <NavLink to="#contact">
-                        <h1 className="text-sm font-cd-semibold dark:text-white" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>Contact</h1>
+                        <h1 className="text-sm font-cd-semibold dark:text-white py-4" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>Contact</h1>
                     </NavLink>
-                    <Link href='https://drive.google.com/file/d/18XyT6wPX2IWdWv0VCqOtwW7ZXYSuAm_g/view?usp=sharing' >
-                        <a target="_blank">
-                            <h1 className="text-sm font-cd-semibold dark:text-white" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>Resume</h1>
+                    <NavLink to=''>
+                        <a href="https://drive.google.com/file/d/18XyT6wPX2IWdWv0VCqOtwW7ZXYSuAm_g/view?usp=sharing" target="_blank" rel="noreferrer">
+                            <h1 className="text-sm font-cd-semibold dark:text-white py-4" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>Resume</h1>
                         </a>
-                    </Link>
+                    </NavLink>
             </div>  
         </div>
     )
@@ -44,7 +47,7 @@ export default function Navbar() {
 
     return (
         <nav className="z-50 flex sticky top-0 w-screen h-20 filter px-8 bg-cool-white/70 dark:bg-slate-800/70 backdrop-blur-md py-4 items-center container justify-between mx-auto px md:px-14 lg:px-24">
-            {open?<MobileNav open={open} setOpen={setOpen}/>:null}
+            {open?<MobileNav open={open} setOpen={setOpen} theme={theme} setTheme={setTheme}/>:null}
 
             <div className="w-3/12 flex items-center">
                 <a className="text-2xl font-cd-regular md:text-2xl lg:text-2xl dark:text-white" href="/">@kaust_av</a>
