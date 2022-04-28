e3dd725a154b6e6ef373bfa8cf851794a6a6d0ad
kaustav927 <kaustav927@gmail.com>
2022-04-28 00:17:39 -0400

    footer2

--- portfolio2.0/components/Footer.tsx
+++ portfolio2.0/components/Footer.tsx
@@ -3,36 +3,45 @@ import React from 'react'
 function Footer() {
   return (
     
-    <footer className="bg-gray-100 text-center lg:text-left">
-      <div className="container p-6 text-gray-800">
-        <div className="grid lg:grid-cols-2 gap-4">
-          <div className="mb-6 md:mb-0">
-            <h5 className="font-medium mb-2 uppercase">Footer text</h5>
+    <div className="container mt-20 justify-between w-10/12 md:w-10/12 lg:w-8/12 h-50 flex items-center py-4">
+      <div className="dark: text-white font-cd-light ">
+       <footer className="text-center lg:text-left">
     
+        <div className="grid lg:grid-cols-3 gap-4">
+          <div className="mb-6 md:mb-0 flex flex-col content-center items-center">
             <p className="mb-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
               molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
               aliquam voluptatem veniam, est atque cumque eum delectus sint!
             </p>
+            <h5 className="font-cd-regualar mb-2 uppercase">Footer text</h5>
           </div>
     
-          <div className="mb-6 md:mb-0">
-            <h5 className="font-medium mb-2 uppercase">Footer text</h5>
-    
+
+          <div className="mb-6 md:mb-0 flex flex-col content-center items-center">
             <p className="mb-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
               molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
               aliquam voluptatem veniam, est atque cumque eum delectus sint!
             </p>
+            <h5 className="font-medium mb-2 uppercase">Footer text</h5>
+          </div>
+
+
+          <div className="mb-6 md:mb-0 flex flex-col content-center items-center">
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+              molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+              aliquam voluptatem veniam, est atque cumque eum delectus sint!
+            </p>
+            <h5 className="font-medium mb-2 uppercase">Footer text</h5>
           </div>
         </div>
+     
+  
+       </footer>
       </div>
-    
-      <div className="text-center text-gray-700 p-4">
-        © 2021 Copyright:
-        <a className="text-gray-800" href="https://tailwind-elements.com/">Tailwind Elements</a>
-      </div>
-    </footer>
+    </div>
   )
 }
 
