d6ec3a80631e52954d5bd22e4e95b4cd823a27af
kaustav927 <ksharm69@uwo.ca>
2022-04-30 00:37:39 -0400

    prettier

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
     <div className="container mt-1 flex justify-center items-center w-screen h-screen px overflow-hidden">
       <div className="flex flex-col">
         <div className="relative w-full max-w-lg mx-auto">
-            <h1 className="z-50 font-cd-semibold  text-2xl my-15 md:text-6xl lg:text-7xl text-center md:text-left dark:text-white">Building beautiful web experiences.</h1>
-            <div className="absolute top-0 -left-4 w-72 h-72 bg-slate-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
-            <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-2000"></div>
-            <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-4000"></div>
+          <h1 className="z-50 font-cd-semibold  text-2xl my-15 md:text-6xl lg:text-7xl text-center md:text-left dark:text-white">
+            Building beautiful web experiences.
+          </h1>
+          <div className="absolute top-0 -left-4 w-72 h-72 bg-slate-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
+          <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-2000"></div>
+          <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-4000"></div>
         </div>
-               
+
         <div className="w-full flex justify-center md:justify-start">
           <button className="mr-6 py-4 bg-theme text-bla font-bold mt-12 flex items-center space-x-3">
-            <span className="font-cd-regular dark:text-white">View my work.</span>
+            <span className="font-cd-regular dark:text-white">
+              View my work.
+            </span>
           </button>
         </div>
       </div>
 
       {/* image */}
     </div>
-   
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -1,35 +1,31 @@
-import  React, {useState} from "react";
-import {FaGithub} from 'react-icons/fa';
-import {SiDevpost} from 'react-icons/si';
-import Link from 'next/link'
-import {ProjectData} from '../components/ProjectData'
-import {FiPlus, FiMinus} from 'react-icons/fi';
+import React, { useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import { SiDevpost } from "react-icons/si";
+import Link from "next/link";
+import { ProjectData } from "../components/ProjectData";
+import { FiPlus, FiMinus } from "react-icons/fi";
 
-import type { NextPage } from 'next'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Head from "next/head";
 
 //import {Parallax, ParallaxLayer} from '@react-spring/parallax'
 const projects: NextPage = () => {
-  
   return (
     <body className="bg-cool-white dark:bg-slate-800 text-black ">
-    <Head>
-      <title>Kaustav Sharma | Projects</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <div className="flex flex-col content-center items-center">
-        <Navbar  /> 
+      <Head>
+        <title>Kaustav Sharma | Projects</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col content-center items-center">
+        <Navbar />
         <div className="container mt-1 flex justify-center items-center w-screen h-screen px overflow-hidden">
-            <div className="flex flex-col">
-                {/* Project Content */}
-            </div>
-         </div>
+          <div className="flex flex-col">{/* Project Content */}</div>
+        </div>
       </div>
-  </body>
-   
-  )
-}
+    </body>
+  );
+};
 
-export default projects
+export default projects;
