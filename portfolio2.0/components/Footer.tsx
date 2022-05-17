762001a4445f60137725f663e11e4dfbdd14d850
kaustav927 <kaustav927@gmail.com>
2022-05-16 23:40:09 -0400

    footer

--- portfolio2.0/components/Footer.tsx
+++ portfolio2.0/components/Footer.tsx
@@ -2,41 +2,17 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="container mt-20 justify-between w-10/12 md:w-10/12 lg:w-8/12 h-50 flex items-center py-4 overflow-x-hidden">
-      <div className="dark: text-white font-cd-light ">
-        <footer className="text-center lg:text-left">
-          <div className="grid lg:grid-cols-3 gap-4">
-            <div className="mb-6 md:mb-0 flex flex-col content-center items-center">
-              <p className="mb-4">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-                atque ea quis molestias. Fugiat pariatur maxime quis culpa
-                corporis vitae repudiandae aliquam voluptatem veniam, est atque
-                cumque eum delectus sint!
-              </p>
-              <h5 className="font-cd-regualar mb-2 uppercase">Footer text</h5>
-            </div>
+    <div className="dark:text-white font-cd-light container mt-20  w-10/12 md:w-10/12 lg:w-8/12 h-50 flex flex-col lg:max-w-screen-lg items-center justify-between py-4 overflow-x-hidden">
+      <div className="w-full flex justify-between ">
+        <div className="border mr-10 px-20 ml-30 h-64 w-1/3 bg-slate-500"></div>
+        <div className="border mx-10 px-20 ml-30 h-64 w-1/3 bg-slate-500"></div>
+        <div className="border ml-10 px-20 ml-30 h-64 w-1/3 bg-slate-500"></div>
+      </div>
 
-            <div className="mb-6 md:mb-0 flex flex-col content-center items-center">
-              <p className="mb-4">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-                atque ea quis molestias. Fugiat pariatur maxime quis culpa
-                corporis vitae repudiandae aliquam voluptatem veniam, est atque
-                cumque eum delectus sint!
-              </p>
-              <h5 className="font-medium mb-2 uppercase">Footer text</h5>
-            </div>
-
-            <div className="mb-6 md:mb-0 flex flex-col content-center items-center">
-              <p className="mb-4">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-                atque ea quis molestias. Fugiat pariatur maxime quis culpa
-                corporis vitae repudiandae aliquam voluptatem veniam, est atque
-                cumque eum delectus sint!
-              </p>
-              <h5 className="font-medium mb-2 uppercase">Footer text</h5>
-            </div>
-          </div>
-        </footer>
+      <div className="w-full flex justify-between mt-4">
+        <div className="border mr-10 px-20 ml-30 h-10 w-1/3 bg-slate-500"></div>
+        <div className="border mx-10 px-20 ml-30 h-10 w-1/3 bg-slate-500"></div>
+        <div className="border ml-10 px-20 ml-30 h-10 w-1/3 bg-slate-500"></div>
       </div>
     </div>
   );
