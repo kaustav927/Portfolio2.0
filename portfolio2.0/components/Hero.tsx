49600fe41f1e4f95385a5db8f997aef2e0f7c01f
kaustav927 <kaustav927@gmail.com>
2022-05-30 17:05:26 -0400

    Hero v2

--- portfolio2.0/components/Hero.tsx
+++ portfolio2.0/components/Hero.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="font-cd-bold container mt-1 flex justify-center items-center w-screen h-screen px overflow-x-hidden">
+    <div className="font-cd-bold mx-4 container mt-1 flex justify-center items-center w-screen h-screen px overflow-x-hidden">
       <div className="flex flex-col">
         <div className="relative w-full max-w-lg mx-auto">
           <h1 className="z-50 font-cd-bold text-2xl my-15 md:text-4xl lg:text-6xl text-center md:text-left dark:text-white">
             Building beautiful web experiences.
           </h1>
-          <div className="absolute top-0 -left-4 w-72 h-72 bg-slate-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob overflow-x-hidden"></div>
-          <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-2000 overflow-x-hidden"></div>
-          <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-4000 overflow-x-hidden"></div>
+          <div className="absolute z-40 top-0 -left-4 w-72 h-72 bg-slate-400 rounded-full mix-blend-multiply dark:mix-blend-screen filter blur-xl opacity-50 animate-blob overflow-x-hidden"></div>
+          <div className="absolute z-40 top-0 -right-4 w-72 h-72 bg-yellow-500 dark:bg-sky-700 rounded-full mix-blend-multiply dark:mix-blend-screen filter blur-xl opacity-50 animate-blob animation-delay-2000 overflow-x-hidden"></div>
+          <div className="absolute z-40 -bottom-8 left-20 w-72 h-72 bg-pink-500 dark:bg-teal-400 rounded-full mix-blend-multiply dark:mix-blend-screen filter blur-xl opacity-50 animate-blob animation-delay-4000 overflow-x-hidden"></div>
         </div>
 
         <div className="w-full flex justify-center md:justify-start">
