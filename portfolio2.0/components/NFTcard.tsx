76623d2ef5a5eb8ce8f4d8a335e6b83003f91fd5
kaustav927 <kaustav927@gmail.com>
2022-10-24 01:23:51 -0400

    new NFT display

--- portfolio2.0/components/NFTcard.tsx
+++ portfolio2.0/components/NFTcard.tsx
@@ -4,10 +4,6 @@ import Link from "next/link";
 import nftData from "../projectData.json";
 
 function NFT() {
-  const openseaLogo =
-    "https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.svg";
-  const stratosLogo = "../public/stratos-logo.png";
-
   interface selectedData {
     index: number | null | any;
   }
@@ -32,38 +28,45 @@ function NFT() {
           >
             {selected === index ? (
               <div
-                className="rounded-md border-2 w-[300px] h-[300px] bg-gray-200 dark:bg-gray-800"
+                className="rounded-md md:w-[300px] md:h-[300px] lg:w-[300px] lg:h-[300px] w-[250px] h-[250px] bg-gray-200 dark:bg-gray-800 "
                 onClick={() => toggle(index)}
               >
-                <span className="mt-6">
+                <div className="flex flex-col justify-center items-center py-6 object-center">
                   {nft.network === "Polygon" ? (
                     <Image
-                      className="conent-center"
+                      className=""
                       src="/logos/polygonLogo.png"
+                      alt="Polygon logo"
                       width={40}
                       height={40}
                     />
                   ) : null}
                   {nft.network === "Arbitrum" ? (
                     <Image
-                      className="content-center"
+                      className=""
                       src="/logos/Arbitrum.png"
-                      width={60}
-                      height={50}
+                      alt="Arbitrum logo"
+                      width={75}
+                      height={60}
                     />
                   ) : null}
-                  <h1>Contract Address:{nft.contractAddress}</h1>
+
                   <Link href={nft.marketplaceURL} passHref>
-                    <a target="_blank" className="mx-3">
-                      <h1>Marketplace Link</h1>
+                    <a target="_blank">
+                      <h1 className="dark:text-black mt-5 dark:bg-cool-white bg-slate-800 text-white px-3 py-2 rounded-md font-cd-regular mb-2">
+                        Marketplace
+                      </h1>
                     </a>
                   </Link>
+
                   <Link href={nft.scanLink} passHref>
-                    <a target="_blank" className="mx-3">
-                      <h1>Block Explorer Link</h1>
+                    <a target="_blank">
+                      <h1 className="dark:text-black mt-5 dark:bg-cool-white bg-slate-800 text-white px-3 py-2 rounded-md font-cd-regular mb-2 ">
+                        Block Explorer
+                      </h1>
                     </a>
                   </Link>
-                </span>
+                </div>
               </div>
             ) : (
               <Image
