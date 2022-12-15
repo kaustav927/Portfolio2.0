da3cf4c5e5577f5e1cedccb605114d17990b8af3
kaustav927 <kaustav927@gmail.com>
2022-12-15 16:01:57 -0500

    google analytics

--- portfolio2.0/pages/_app.tsx
+++ portfolio2.0/pages/_app.tsx
@@ -1,12 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ThemeProvider} from 'next-themes'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url:string) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return(
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
-    </ThemeProvider>
-    
+    </ThemeProvider>    
   ) 
 }
 export default MyApp
